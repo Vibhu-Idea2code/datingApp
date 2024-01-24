a65fc7d3d453d2582c9e29fff8b1cd2e47db93fc
Vibhu-Idea2code <vs.idea2code@gmail.com>
2024-01-24 18:02:38 +0530

    inital

--- src/models/index.js
+++
@@ -1,9 +0,0 @@
-module.exports.Admin = require("./admin.model");
-module.exports.User = require("./users.model");
-module.exports.Location = require("./location.model");
-// module.exports.Number = require("./number.model");
-module.exports.Hobbies = require("./interest.model");
-module.exports.Sexual = require("./sexual.oriantetaion.model");
-module.exports.Pets = require("./pets.model");
-module.exports.Sign = require("./zodiac.sign.model");
-module.exports.Plan = require("./plan.model");
